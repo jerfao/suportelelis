0a33f4138443d14143d4406b6bbd834594b928bf
Jeferson <jerfao@hotmail.com>
2018-06-18 23:03:36 -0300

    Update install_proxy.sh
    Update função ZABBIXSEVER

--- install_proxy.sh
+++ install_proxy.sh
@@ -14,7 +14,7 @@ clear
    echo "[ 6 ] Sair"
    echo
    echo -n "Qual a opcao desejada ? "
-   echo "------------------------------------------"
+
    read opcao
    case $opcao in
       1) ZabbixServer;;
@@ -35,22 +35,22 @@ echo "desinstaldo o Zabbix"
 Backup() {
 echo "Especifique o local a ser backupeado"
 read local
- 
- 
+
+
 echo "+++++++++++++++++++++++++++++++++++++++"
 echo
- 
+
 echo "Especifique nome do backup =)"
 read nome
- 
+
 echo
- 
+
 echo
 echo "+++++++++++++++++++++++++++++++++++++++"
- 
+
 echo "Especifique o destino do backup =)"
 read destino
- 
+
 sudo tar cvf $destino/$nome.tar $local
 cd $destino
 ls $nome
@@ -65,6 +65,145 @@ Menu
 
 }
 
+
+ZabbixAgent(){
+
+#outro teste não homologado
+
+echo "Instalando o Agent Zabbix"
+#Instalando o Zabbix Agent:
+apt-get install zabbix-agent
+service zabbix-agent start
+systemctl start zabbix-agent
+
+#apt-get -y install zabbix-agent sysv-rc-conf
+#sysv-rc-conf zabbix-agent on
+#myip=$(hostname -I)
+#sed -i 's/Server=127.0.0.1/Server='$myip'/' /etc/zabbix/zabbix_agentd.conf
+#sed -i 's/ServerActive=127.0.0.1/ServerActive='$myip'/' /etc/zabbix/zabbix_agentd.conf
+#HOSTNAME=`hostname` && sed -i "s/Hostname=Zabbix\ server/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
+
+sed -i 's/# DebugLevel=3/ DebugLevel=3/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/# User=zabbix/ User=zabbix/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/# AllowRoot=0/ AllowRoot=1/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/# EnableRemoteCommands=0/ EnableRemoteCommands=1/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/# LogRemoteCommands=0/ LogRemoteCommands=1/' /etc/zabbix/zabbix_agentd.conf
+
+service zabbix-agent restart
+#---------------------------------------------------------------------------------------------------
+
+sleep 2
+Menu
+}
+
+ZabbixServer() {
+
+groupadd zabbix
+useradd -g zabbix -s /bin/false zabbix
+wget -qO- https://goo.gl/NJNoqi | bash
+
+apt-get update && apt-get upgrade
+cd /tmp/
+
+wget http://repo.zabbix.com/zabbix/3.4/debian/pool/main/z/zabbix-release/zabbix-release_3.4-1+stretch_all.deb
+dpkg -i zabbix-release_3.4-1+stretch_all.deb
+apt-get update -y
+apt-get install -y zabbix-server-mysql zabbix-frontend-php zabbix-agent zabbix-get zabbix-sender php7.0-bcmath php7.0-mbstring php-sabre-xml nmap sudo snmp-mibs-downloader snmpd
+
+/etc/init.d/snmpd restart
+#Vamos criar uma base de dados chamada zabbix e um usuário também chamado de zabbix no MariaDB.
+
+# Criando a base de dados zabbix
+echo "digite a senha do banco"
+read  DBsenha
+                echo "Creating zabbix database..."
+                mysql -u root -p$DBsenha -e "create database zabbix character set utf8 collate utf8_bin";
+                sleep 1
+                echo "Creating zabbix user at MariaDB SGBD..."
+                mysql -u root -p$DBsenha -e "create user 'zabbix'@'localhost' identified by '$DBsenha'";
+                sleep 1
+                echo "Making zabbix user the owner to zabbix database..."
+                mysql -u root -p$DBsenha -e "grant all privileges on zabbix.* to 'zabbix'@'localhost' with grant option";
+                mysql -u root -p$DBsenha -e "quit";
+
+echo  "banco criado verifique /etc/zabbix"
+sed -i 's/# DBPassword=/DBPassword='$DBsenha'/' /etc/zabbix/zabbix_server.conf
+
+echo "Populando o Banco de Dados,este procedimento demora um pouco, por favor Aguarde!"
+
+zcat /usr/share/doc/zabbix-server-mysql/create.sql.gz | mysql -uzabbix -p$DBsenha zabbix
+
+myip=$(hostname -I)
+echo "seu Ip: '$myip'"
+
+echo "Banco Populado acesse http://'$myip'/ZABBIX"
+#Agora vamos editar o arquivo zabbix_server.conf para informar os dados para conexão com o MySQL.
+
+# vim /etc/zabbix/zabbix_server.conf
+#...
+#DBHost=localhost
+#...
+#DBName=zabbix
+#...
+#DBUser=zabbix
+#...
+#DBPassword=SENHA-USUARIO-ZABBIX
+#...
+#mudando o timezone
+
+
+sed -i 's/# DBHost=localhost/ DBHost=localhost/' /etc/zabbix/zabbix_server.conf
+
+#config basica do Agent
+#sed -i 's/Server=127.0.0.1/Server=$myip/' /etc/zabbix/zabbix_agentd.conf
+#sed -i 's/ServerActive=127.0.0.1/ServerActive=$myip/' /etc/zabbix/zabbix_agentd.conf
+#HOSTNAME=`hostname` && sed -i "s/Hostname=Zabbix\ server/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
+#service zabbix-agent restart
+
+sed -i 's/# php_value date.timezone Europe\/Riga/php_value date.timezone America\/Sao_Paulo/' /etc/apache2/conf-enabled/zabbix.conf
+
+sed -i 's/# php_value date.timezone Europe\/Riga/php_value date.timezone America\/Sao_Paulo/' /etc/zabbix/apache.conf
+
+# iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 10050 -j ACCEPT
+echo "zabbix ALL=(ALL:ALL) NOPASSWD:/usr/bin/nmap" >> /etc/sudoers
+
+#apt install php7.0-bcmath php7.0-mbstring php-sabre-xml -y
+
+/etc/init.d/apache2 restart
+
+#iniciando o Zabbix Server e o Agente.
+
+systemctl enable zabbix-server
+systemctl enable zabbix-agent
+/etc/init.d/zabbix-server restart
+/etc/init.d/zabbix-agent restart
+
+#ZabbixAgent
+
+clear
+echo "Instalação Zabbix Server Concluida!.."
+sleep 4
+
+Menu
+
+}
+
+
+Downloads() {
+
+#Versão teste
+VERSAO=3.2.0
+export VERSAO
+cd /tmp/
+wget  wget http://repo.zabbix.com/zabbix/3.4/debian/pool/main/z/zabbix-release/zabbix-release_3.4-1+stretch_all.deb
+dpkg -i zabbix-release_3.4-1+stretch_all.deb
+
+ls
+echo "Download Realizado com Sucesso!"
+sleep 3
+
+Menu
+}
 ZabbixProxy() {
 echo "Instalando o Zabbix Proxy"
 sleep 1
@@ -92,17 +231,17 @@ mkdir /var/lib/zabbix
 chown zabbix. –R /var/lib/zabbix
 
 # Habilitando execução de comandos via Zabbix
-	sed -i 's/# EnableRemoteCommands=0*/EnableRemoteCommands=1/' /etc/zabbix/zabbix_agentd.conf
-	sed -i 's/# ProxyOfflineBuffer=1*/ProxyOfflineBuffer=24/' /etc/zabbix/zabbix_agentd.conf
-	sed -i 's/# ConfigFrequency=*/ConfigFrequency=3600/' /etc/zabbix/zabbix_agentd.conf
-	sed -i 's/# DataSenderFrequency=1*/DataSenderFrequency=1/' /etc/zabbix/zabbix_agentd.conf
-	sed -i 's/# DBName=Zabbix Server*/DBName=\/var\/lib\/sqlite3\/zabbix.db/' /etc/zabbix/zabbix_agentd.conf
-	
-	
-	sed -i 's/# FpingLocation=\/usr\/sbin\/fping/FpingLocation=\/usr\/bin\/fping/' /etc/zabbix/zabbix_proxy.conf
+        sed -i 's/# EnableRemoteCommands=0*/EnableRemoteCommands=1/' /etc/zabbix/zabbix_agentd.conf
+        sed -i 's/# ProxyOfflineBuffer=1*/ProxyOfflineBuffer=24/' /etc/zabbix/zabbix_agentd.conf
+        sed -i 's/# ConfigFrequency=*/ConfigFrequency=3600/' /etc/zabbix/zabbix_agentd.conf
+        sed -i 's/# DataSenderFrequency=1*/DataSenderFrequency=120/' /etc/zabbix/zabbix_agentd.conf
+        sed -i 's/# DBName=Zabbix Server*/DBName=\/var\/lib\/sqlite3\/zabbix.db/' /etc/zabbix/zabbix_agentd.conf
 
-	
-	
+
+        sed -i 's/# FpingLocation=\/usr\/sbin\/fping/FpingLocation=\/usr\/bin\/fping/' /etc/zabbix/zabbix_proxy.conf
+
+
+
 # populando sqlite3 não obrigratorio
 sqlite3 /var/lib/sqlite3/zabbix.db < schema.sql
 
@@ -138,138 +277,12 @@ sleep 2
 Menu
 }
 
-ZabbixAgent(){
-
-#outro teste não homologado
-
-echo "Instalando o Agent Zabbix"
-#Instalando o Zabbix Agent:
-apt-get install zabbix-agent
-service zabbix-agent start
-systemctl start zabbix-agent
-
-#apt-get -y install zabbix-agent sysv-rc-conf
-#sysv-rc-conf zabbix-agent on
-#myip=$(hostname -I)
-#sed -i 's/Server=127.0.0.1/Server='$myip'/' /etc/zabbix/zabbix_agentd.conf
-#sed -i 's/ServerActive=127.0.0.1/ServerActive='$myip'/' /etc/zabbix/zabbix_agentd.conf
-#HOSTNAME=`hostname` && sed -i "s/Hostname=Zabbix\ server/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
-service zabbix-agent restart
-#---------------------------------------------------------------------------------------------------
-
-sleep 2
-Menu
-}
-
-ZabbixServer() {
-
-groupadd zabbix
-useradd -g zabbix -s /bin/false zabbix
-
-apt update && apt upgrade
-cd /tmp
-
-wget http://repo.zabbix.com/zabbix/3.4/debian/pool/main/z/zabbix-release/zabbix-release_3.4-1+stretch_all.deb
-dpkg -i zabbix-release_3.4-1+stretch_all.deb
-apt-get update -y
-apt-get install -y zabbix-server-mysql zabbix-frontend-php zabbix-agent zabbix-get 
-
-#Vamos criar uma base de dados chamada zabbix e um usuário também chamado de zabbix no MariaDB.
-
-# Criando a base de dados zabbix
-echo "digete a senha do banco"
-read  DBsenha
-                echo "Creating zabbix database..."
-                mysql -u root -p$DBsenha -e "create database zabbix character set utf8 collate utf8_bin";
-                sleep 1
-                echo "Creating zabbix user at MariaDB SGBD..."
-                mysql -u root -p$DBsenha -e "create user 'zabbix'@'localhost' identified by '$DBsenha'";
-                sleep 1
-                echo "Making zabbix user the owner to zabbix database..."
-                mysql -u root -p$DBsenha -e "grant all privileges on zabbix.* to 'zabbix'@'localhost' with grant option";
-				mysql -u root -p$DBsenha -e "quit";
-				
-echo  "banco criado verifique /etc/zabbix"
-sed -i 's/# DBPassword=/DBPassword='$DBsenha'/' /etc/zabbix/zabbix_server.conf
-
-echo "Populando o Banco de Dados,este procedimento demora um pouco, por favor Aguarde!"
-#zcat /usr/share/doc/zabbix-server-mysql/create.sql.gz | mysql -uzabbix -p zabbix
-zcat /usr/share/doc/zabbix-server-mysql/create.sql.gz | mysql -uzabbix -p zabbix
-
-myip=$(hostname -I)
-echo "seu Ip: $myip"
-
-echo "Banco Populado acesse http://$myip/ZABBIX"
-#Agora vamos editar o arquivo zabbix_server.conf para informar os dados para conexão com o MySQL.
-
-# vim /etc/zabbix/zabbix_server.conf
-#...
-#DBHost=localhost
-#...
-#DBName=zabbix
-#...
-#DBUser=zabbix
-#...
-#DBPassword=SENHA-USUARIO-ZABBIX
-#...
-#mudando o timezone
-
-
-sed -i 's/# DBHost=localhost/ DBHost=localhost/' /etc/zabbix/zabbix_server.conf
-
-#config basica do Agent
-sed -i 's/Server=127.0.0.1/Server=$myip/' /etc/zabbix/zabbix_agentd.conf
-sed -i 's/ServerActive=127.0.0.1/ServerActive=$myip/' /etc/zabbix/zabbix_agentd.conf
-HOSTNAME=`hostname` && sed -i "s/Hostname=Zabbix\ server/Hostname=$HOSTNAME/" /etc/zabbix/zabbix_agentd.conf
-#service zabbix-agent restart
-
-sed -i 's/# php_value date.timezone Europe\/Riga/php_value date.timezone America\/Sao_Paulo/' /etc/apache2/conf-enabled/zabbix.conf
-
-sed -i 's/# php_value date.timezone Europe\/Riga/php_value date.timezone America\/Sao_Paulo/' /etc/zabbix/apache.conf
-
-apt install -y php7.0-bcmath php7.0-mbstring php-sabre-xml
-
-/etc/init.d/apache2 restart
-
-#iniciando o Zabbix Server e o Agente.
-
-systemctl enable zabbix-server
-systemctl enable zabbix-agent
-/etc/init.d/zabbix-server restart
-/etc/init.d/zabbix-agent restart
-
-#ZabbixAgent
-
-clear
-echo "Instalação Zabbix Server Concluida!.."
-sleep 4
-
-Menu
-
-}
-
-
-Downloads() {
-
-#Versão teste 
-VERSAO=3.2.0
-export VERSAO
-cd /tmp/
-wget  wget http://repo.zabbix.com/zabbix/3.4/debian/pool/main/z/zabbix-release/zabbix-release_3.4-1+stretch_all.deb
-dpkg -i zabbix-release_3.4-1+stretch_all.deb
-
-ls
-echo "Download Realizado com Sucesso!"
-sleep 3
-
-Menu
-}
 
 InstalarProxys() {
 
 # Creating system user
-# 	groupadd zabbix
-# 	useradd -g zabbix -s /bin/false zabbix
+#       groupadd zabbix
+#       useradd -g zabbix -s /bin/false zabbix
 #Adionando User Zabbix
 adduser zabbix --shell /bin/false
 
